8b9ac5f25458b20bf40c9d38052858d701cce9a1
kukutt <pan3yao@gmail.com>
2021-02-10 14:15:23 +0000

    hackrf 发送脚本

---
+++ src/shell/usb.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+(while true; do cat $2; done) | csdr convert_i16_f \
+  | csdr fir_interpolate_cc 10 | csdr dsb_fc \
+  | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | csdr convert_f_s8 \
+  | hackrf_transfer -f $1 -s 480000 -x 20 -t -
+
