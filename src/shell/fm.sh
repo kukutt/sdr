efa77a0c3e4f5d0398817b7d4461dd32a684b4cd
kukutt <pan3yao@gmail.com>
2021-02-10 15:08:44 +0000

    添加fm调制

---
+++ src/shell/fm.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+(while true; do cat $2; done) | csdr convert_i16_f \
+  | csdr fir_interpolate_cc 10 | csdr fmmod_fc \
+  | csdr convert_f_s8 \
+  | hackrf_transfer -f $1 -s 480000 -x 20 -t -
+
